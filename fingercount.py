d1c2fb93ebc715853b48b55cda1706cede182daf
Saurabh Grewal <saurabhgrewal718@gmail.com>
2021-06-23 18:41:10 +0530

    the finger counter done

---
+++ fingercount.py
@@ -0,0 +1,54 @@
+import cv2
+import time
+import os
+import handtrackingModule as htm
+
+wCam,hCam = 648,480
+
+cap = cv2.VideoCapture(0)
+cap.set(3,wCam)
+cap.set(4,hCam)
+
+folderpath = "fingers"
+myList = os.listdir(folderpath)
+print(myList)
+
+detector = htm.handDetector(detectionCon=0.75)
+
+overlay_list = []
+for im in myList:
+    image = cv2.imread(f'{folderpath}/{im}')
+    overlay_list.append(image)
+print("the len is : ",len(overlay_list))
+pTime = 0
+
+tipIds = [4,8,12,16,20]
+while True:
+    success,img = cap.read()
+    img = detector.findHands(img)
+    imlist = detector.findPosition(img,draw=False)
+
+    if len(imlist) !=0:
+        fingers = []
+        if imlist[tipIds[0]][1] < imlist[tipIds[0]-1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        for id in range(1,5):
+            if imlist[tipIds[id]][2] < imlist[tipIds[id]-2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        print(fingers)
+
+    h,w,c = overlay_list[1].shape
+    img[0:h,0:w] = overlay_list[1]
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img,f'FPS : {int(fps)}',(400,70),cv2.FONT_HERSHEY_PLAIN,3,(255,0,0),3)
+    cv2.imshow('Image',img)
+    cv2.waitKey(1)
--- volumeControl.py
+++ volumeControl.py
@@ -12,13 +12,10 @@ interface = devices.Activate(
 volume = cast(interface, POINTER(IAudioEndpointVolume))
 # volume.GetMute()
 # volume.GetMasterVolumeLevel()
-#
-volrange = volume.GetVolumeRange()
 
+volrange = volume.GetVolumeRange()
 minVol = volrange[0]
 maxVol = volrange[1]
-
-
 detector = htm.handDetector(detectionCon=0.8)
 wCam,hCam = 1280,720
 cap = cv2.VideoCapture(0)
@@ -42,7 +39,6 @@ while True:
         x2,y2 = lmList[8][1],lmList[8][2]
         cx,cy = (x1+x1)//2,(y1+y2)//2
 
-
         cv2.circle(img,(x1,y1),15,(255,0,0),cv2.FILLED)
         cv2.circle(img,(x2,y2),15,(255,0,0),cv2.FILLED)
         cv2.line(img,(x1,y1),(x2,y2),(255,9,255),3)
@@ -57,16 +53,10 @@ while True:
         cv2.rectangle(img,(50,150),(85,400),(0,255,0),3)
         cv2.rectangle(img,(50,int(volBar)),(85,400),(0,255,0),cv2.FILLED)
 
-
-
-
         if length<20:
             cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
         if length>140:
             cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
-
-
-
 
     cv2.imshow("IMG",img)
     cv2.waitKey(1)
